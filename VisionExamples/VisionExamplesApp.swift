942148f997214100c0d928f9083fd000a0229269
Gaurav Kumar <gauravgupta0216@gmail.com>
2024-03-05 15:08:54 +0530

    Initial Commit

---
+++ VisionExamples/VisionExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionExamplesApp.swift
+//  VisionExamples
+//
+//  Created by Gaurav Kumar on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

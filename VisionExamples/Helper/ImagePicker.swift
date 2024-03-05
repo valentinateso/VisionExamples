d495abca2f579dc19e8fb354e5803054a5ba2689
Gaurav Kumar <gauravgupta0216@gmail.com>
2024-03-05 15:29:25 +0530

    Added Image Picker

--- VisionExamples/Helper/ImagePicker.swift
+++ VisionExamples/Helper/ImagePicker.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @SwiftUI.Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    let imagePickerType: PickerType
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        if imagePickerType == .camera {
+            picker.sourceType = .camera
+        }
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        //No need to update
+    }
+}
+
+enum PickerType {
+    case camera
+    case photoLibrary
+}
